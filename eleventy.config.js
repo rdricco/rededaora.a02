f2b082f36640ffc255274afdd0ad973b3f7afbab
renato ricco <renato@ricco.dev.br>
2026-03-04 00:47:04 -0300

    feat: Add EleventyHtmlBasePlugin and configure a pathPrefix for correct base URL resolution.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,12 @@
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+
 export default function(eleventyConfig) {
+  // Automaticamente conserta links que começam com `/` para incluírem o pathPrefix
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
   // Return your Object options:
   return {
+    pathPrefix: "/rededaora.a02/",
     dir: {
       input: "src",
       output: "_site"
